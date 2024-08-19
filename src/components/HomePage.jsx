0d537d0e23d01e98161ef08b0b6ee0e58e0ac0aa
Christopher <114704469+Blade-24@users.noreply.github.com>
2024-08-18 22:39:47 -0600

    change URL for API

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,9 @@ function HomePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/${empresa}/dashboard`);
+        const response = await fetch(
+          `https://sales-manager-api.onrender.com/${empresa}/dashboard`
+        );
         const result = await response.json();
         setData(result);
       } catch (error) {
@@ -25,7 +27,9 @@ function HomePage() {
 
     const fetchVentas = async () => {
       try {
-        const response = await fetch(`/api/${name}/ventas`);
+        const response = await fetch(
+          `https://sales-manager-api.onrender.com/${name}/ventas`
+        );
         const result = await response.json();
         setVentas(result);
       } catch (error) {
