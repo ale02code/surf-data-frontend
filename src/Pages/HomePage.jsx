121df9ae035c5795379b2637e8a86ce80e9ba153
Christopher <114704469+Blade-24@users.noreply.github.com>
2024-12-20 23:00:36 -0600

    remove unnecessary class

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -156,7 +156,7 @@ function HomePage() {
             </article>
             <div className="overflow-hidden h-60">
               <img
-                className="h-full min-w-max object-cover"
+                className="h-full object-cover"
                 src={OctopusImg}
                 alt="Octopus Image"
               />
