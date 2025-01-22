5970a5355af2f509e65460c30089926d93920a52
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-01-21 23:16:17 -0600

    fix: enhance layout and styling for MenuDashboard and SalesDashboardView components

--- src/components/MenuDashboard.jsx
+++ src/components/MenuDashboard.jsx
@@ -24,38 +24,40 @@ function MenuDashboard() {
   };
 
   return (
-    <header className="h-full min-w-56 relative text-white p-2 overflow-hidden border-r border-gray-300 bg-gray-100">
-      <nav className="h-full flex flex-col justify-between">
-        <section>
-          <div className="text-lg font-semibold">
-            <div className="mt-2 mb-5">
-              <h1 className="text-2xl font-bold text-carbon-blue text-center uppercase">
-                Surf Data
-              </h1>
-            </div>
-            {sections.map((section) => (
-              <div
-                key={section.label}
-                className="p-2 flex items-center gap-2 mb-1 hover:bg-gray-200 cursor-pointer"
-                onClick={() => handleSectionView(section.label)}
-              >
-                <img src={section.img} alt={section.img + " icon"} />
-                <p className="text-carbon-blue">{section.label}</p>
+    <header className="h-full w-56 overflow-hidden">
+      <div className="h-full flex flex-col justify-between fixed">
+        <nav className="h-full flex flex-col justify-between bg-gray-100 text-white p-4 overflow-hidden border-r border-gray-300">
+          <section>
+            <div className="text-lg font-semibold">
+              <div className="mt-2 mb-5">
+                <h1 className="text-2xl font-bold text-carbon-blue text-center uppercase">
+                  Surf Data
+                </h1>
               </div>
-            ))}
-          </div>
-        </section>
-        <section>
-          <a href="https://surfdata.online">
-            <div className="flex items-center gap-2 hover:bg-red-100 p-2 cursor-pointer">
-              <img src={logoutIcon} alt="Log Out Icon" />
-              <p className="text-red-500 text-lg font-semibold">
-                Cerrar sesión
-              </p>
+              {sections.map((section) => (
+                <div
+                  key={section.label}
+                  className="p-2 flex items-center gap-2 mb-1 hover:bg-gray-200 cursor-pointer"
+                  onClick={() => handleSectionView(section.label)}
+                >
+                  <img src={section.img} alt={section.img + " icon"} />
+                  <p className="text-carbon-blue">{section.label}</p>
+                </div>
+              ))}
             </div>
-          </a>
-        </section>
-      </nav>
+          </section>
+          <section>
+            <a href="https://surfdata.online">
+              <div className="flex items-center gap-2 hover:bg-red-100 p-2 cursor-pointer">
+                <img src={logoutIcon} alt="Log Out Icon" />
+                <p className="text-red-500 text-lg font-semibold">
+                  Cerrar sesión
+                </p>
+              </div>
+            </a>
+          </section>
+        </nav>
+      </div>
     </header>
   );
 }
